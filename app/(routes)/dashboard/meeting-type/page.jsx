40f76e6320b5f0e71666c21366c2d4ad5c2d1337
Avinash-1-10 <avinashyede110@gmail.com>
2024-04-15 22:39:32 +0530

    meeting type page added

--- app/(routes)/dashboard/meeting-type/page.jsx
+++ app/(routes)/dashboard/meeting-type/page.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Input } from "../../../../components/ui/input";
+import MeetingEventList from './_components/MeetingEventList'
 
 const MeetingType = () => {
-  return (
-    <div>MeetingType</div>
-  )
-}
 
-export default MeetingType
+  return (
+    <div className="p-5">
+      <div className="flex flex-col gap-5">
+        <h2 className="font-bold text-2xl">Meeting Event Type</h2>
+        <Input placeholder="Search" className="max-w-xs" />
+        <hr />
+      </div>
+      <MeetingEventList/>
+    </div>
+  );
+};
+
+export default MeetingType;
